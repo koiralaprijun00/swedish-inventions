177675e513a3d78303d0b6c5696b9fa5d0c286e2
Prijun Koirala <prijun@kth.se>
2025-02-16 20:40:29 +0100

    Uninstalled esLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,13 @@
-// .eslintrc.js
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
-  // ...
-  parserOptions: {
-    parser: "@babel/eslint-parser" // Or any other parser you want to use
+  extends: ["next/core-web-vitals"],
+  rules: {
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off"
+  },
+  settings: {
+    next: {
+      rootDir: "packages/my-app/"
+    }
   }
-  // ...
-}
+};
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,19 @@
-/** @type {import('eslint').ESLintConfig} */
-export default {
-  extends: ["next"]
-}
+import { FlatCompat } from "@eslint/eslintrc"
+import { join } from "path"
+
+const compat = new FlatCompat({
+  baseDirectory: join(process.cwd())
+})
+
+const configArray = compat.config({
+  extends: ["next/core-web-vitals"],
+  rules: {
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off"
+  },
+  settings: {
+    next: {
+      rootDir: "packages/my-app/"
+    }
+  }
+})
