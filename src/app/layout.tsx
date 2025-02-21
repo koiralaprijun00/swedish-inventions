5cfae7c49b789edf44cbff757392983d73ac1dc6
Prijun Koirala <prijun@kth.se>
2025-02-21 12:17:13 +0100

    Added icon for Apple Touch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
-import { GoogleAnalytics } from "@next/third-parties/google"
+import { GoogleAnalytics } from "@next/third-parties/google";
+import Link from "next/link";
 import "./styles/globals.css";
 
 export const metadata: Metadata = {
   title: "Swedish Inventions – Innovations That Changed the World",
   description: "Explore groundbreaking Swedish inventions that shaped technology, medicine, and everyday life. Discover Sweden’s legacy of innovation."
 };
-
 
 export default function RootLayout({
   children,
@@ -17,17 +17,15 @@ export default function RootLayout({
   return (
     <html lang='en' suppressHydrationWarning>
       <head>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4708248697764153"
-     crossOrigin="anonymous"></script>
-      <meta name="google-adsense-account" content="ca-pub-4708248697764153" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4708248697764153"
+          crossOrigin="anonymous"></script>
+        <meta name="google-adsense-account" content="ca-pub-4708248697764153" />
       </head>
       <GoogleAnalytics gaId="G-WJ47ZHVZ27" />
-      <body
-        className={`antialiased`}
-      >
+      <body className={`antialiased`}>
         {children}
       </body>
-      
     </html>
   );
 }
