6546beef1f5c43116a2cafb59cdf92c9282bf9de
Prijun Koirala <prijun@kth.se>
2025-02-23 14:48:13 +0100

    Improved About Us Page

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -5,8 +5,8 @@ export default function AboutPage() {
   const t = useTranslations("Translations")
 
   return (
-    <main className="container mt-12">
-      <p className="mb-6 w-1/2 text-gray-950 dark:text-white leading-8">
+    <main className="mt-12">
+      <p className="mb-6 w-full md:w-1/2 leading-8 text-gray-800 dark:text-white dark:!text-white">
         {t("aboutPage")}
       </p>
     </main>
