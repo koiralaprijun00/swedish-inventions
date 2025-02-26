2ee68b4190be4cb2f7789fe335c4c088bc95ed47
Prijun Koirala <prijun@kth.se>
2025-02-26 12:08:50 +0100

    Attempt to reduce LCP for Header

--- src/app/[locale]/components/Header.tsx
+++ src/app/[locale]/components/Header.tsx
@@ -1,35 +1,35 @@
 "use client"
 
-import React from "react"
+import React, { memo } from "react"
 import { useTranslations } from "next-intl"
 
-export default function Header() {
+const Header = memo(function Header() {
   const t = useTranslations("Translations")
 
   return (
     <header className="text-start mb-24 w-full max-w-5xl text-primaryBlue">
     <div className="py-2 md:py-8 text-left">
-  <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-extrabold tracking-tight">
+  <h1 className="prose text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-extrabold tracking-tight">
     {t("title")}{" "}
     <span
-      className="font-bold bg-clip-text text-transparent bg-gradient-to-r from-[#005293] to-[#fecb00]"
-      style={{ textShadow: "1px 1px 0px rgba(19, 90, 172, 0.25)" }}
+      className="prose font-bold bg-clip-text text-transparent bg-gradient-to-r from-[#005293] to-[#fecb00]"
     >
       {t("inventions")}
     </span>{" "}
     {t("and")}{" "}
     <span
-      className="font-bold bg-clip-text text-transparent bg-gradient-to-r from-[#005293] to-[#fecb00]"
-      style={{ textShadow: "1px 1px 0px rgba(19, 90, 172, 0.25)" }}
+      className="prose font-bold bg-clip-text text-transparent bg-gradient-to-r from-[#005293] to-[#fecb00]"
     >
       {t("innovations")}
     </span>
   </h1>
 </div>
 
-      <p className="text-gray-600 text-base sm:text-lg md:text-xl leading-relaxed md:leading-loose">
+      <p className="prose text-gray-600 text-base sm:text-lg md:text-xl leading-relaxed md:leading-loose">
         {t("headerText")} <span className="bg-amber-300 p-1 md:p-2 text-primaryBlue font-bold leading-relaxed md:leading-loose">{t("peopleOfSweden")}</span>
       </p>
     </header>
   )
-}
+})
+
+export default Header
